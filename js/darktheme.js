8140bcaa8cd39b4c0a391d4cfb817af6b9f73851
Ana Almeida <aninhaalmeidav@gmail.com>
2023-03-28 13:33:50 -0300

    darkmode  aplicado

--- js/darktheme.js
+++ js/darktheme.js
@@ -1,29 +1,30 @@
-const changeThemeBtn = document.querySelector("#change-theme");
 
-// Toggle dark mode
-function toggleDarkMode() {
+
+  const changeThemeBtn = document.querySelector("#change-theme");
+
+  // Toggle dark mode
+  function toggleDarkMode() {
     document.body.classList.toggle("dark");
-}
+  }
 
-// Load light or dark mode
-function loadTheme() {
+  // Load light or dark mode
+  function loadTheme() {
     const darkMode = localStorage.getItem("dark");
 
     if (darkMode) {
-        toggleDarkMode();
+      toggleDarkMode();
     }
-}
+  }
 
-loadTheme();
+  loadTheme();
+  console.log(changeThemeBtn)
 
-changeThemeBtn.addEventListener("change", function () {
+  changeThemeBtn.addEventListener("click", function () {
     toggleDarkMode();
-
+  
     // Save or remove dark mode from localStorage
     localStorage.removeItem("dark");
-
     if (document.body.classList.contains("dark")) {
-        localStorage.setItem("dark", 1);
+      localStorage.setItem("dark", 1);
     }
-});
-
+  });
