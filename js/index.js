362d5c5b48920055de72450eec8b3c09562bdfc3
Bruno Araújo <bruno.ar.avl@gmail.com>
2023-04-10 18:09:47 -0300

    adicionando as funcionalidades aos botões excluir e arquivar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (async () => {
-    const dados = await (await fetch('./dados.json')).json();
+    let dados = await (await fetch('./dados.json')).json();
     const extrairMarcadoresUnicos = (dados) => {
         const marcadoresUnicos = [];
         for(let index = 0; index < dados.length; index++){
@@ -46,10 +46,14 @@
     }
 
     const formatarNotas = (dados) => {
+        document.querySelector('.notas-container .row').innerHTML = "";
         for(let index = 0; index < dados.length; index++){
-            const { id, titulo, conteudo, cor, marcadores } = dados[index];
-            renderizarNotas(id, titulo, conteudo, cor, marcadores);
+            const { id, titulo, conteudo, cor, marcadores, arquivado, excluido } = dados[index];
+            if(arquivado === false && excluido === false) {
+                renderizarNotas(id, titulo, conteudo, cor, marcadores);
+            }
         }
+        atribuirEventos();
     }
 
     const filtrarNotasPorMarcador = (dados, marcador) => {
@@ -61,6 +65,27 @@
             const { id, titulo, conteudo, cor, marcadores } = dadosFiltrados[index];
             renderizarNotas(id, titulo, conteudo, cor, marcadores);
         }
+        atribuirEventos();
+    }
+
+    const filtrarNotasPorArquivado = (dados) => {
+        const dadosFiltrados = dados.filter(dado => dado.arquivado === true);
+        document.querySelector('.notas-container .row').innerHTML = "";
+        for(let index = 0; index < dadosFiltrados.length; index++){
+            const { id, titulo, conteudo, cor, marcadores } = dadosFiltrados[index];
+            renderizarNotas(id, titulo, conteudo, cor, marcadores);
+        }
+        atribuirEventos();
+    }
+
+    const filtrarNotasPorExcluido = (dados) => {
+        const dadosFiltrados = dados.filter(dado => dado.excluido === true);
+        document.querySelector('.notas-container .row').innerHTML = "";
+        for(let index = 0; index < dadosFiltrados.length; index++){
+            const { id, titulo, conteudo, cor, marcadores } = dadosFiltrados[index];
+            renderizarNotas(id, titulo, conteudo, cor, marcadores);
+        }
+        atribuirEventos();
     }
 
     const abrirNota = (id) => {
@@ -87,41 +112,103 @@
         document.querySelector('.nota-aberta .notas').classList.remove('nota-laranja');
         document.querySelector('.nota-aberta .nota-marcadores').innerHTML = '';
     }
+    const atribuirEventos = () => {
+
+        document.querySelectorAll('.js-marcador').forEach(function(marcador){
+            marcador.addEventListener('click', function(event){
+                let nomeMarcador = '';
+                if(event.target.tagName === 'LI'){
+                    nomeMarcador = event.target.querySelector('a').textContent;
+                } else {
+                    nomeMarcador = event.target.parentElement.querySelector('a').textContent;
+                }
+                filtrarNotasPorMarcador(dados, nomeMarcador);
+            }, false);
+        });
+
+        document.querySelectorAll('.js-menu-arquivo').forEach(function(marcador){
+            marcador.addEventListener('click', function(event){
+                let nomeMarcador = '';
+                if(event.target.tagName === 'LI'){
+                    nomeMarcador = event.target.querySelector('a').textContent;
+                } else {
+                    nomeMarcador = event.target.parentElement.querySelector('a').textContent;
+                }
+                filtrarNotasPorArquivado(dados);
+            }, false);
+        });
+
+        document.querySelectorAll('.js-menu-lixeira').forEach(function(marcador){
+            marcador.addEventListener('click', function(event){
+                let nomeMarcador = '';
+                if(event.target.tagName === 'LI'){
+                    nomeMarcador = event.target.querySelector('a').textContent;
+                } else {
+                    nomeMarcador = event.target.parentElement.querySelector('a').textContent;
+                }
+                filtrarNotasPorExcluido(dados, nomeMarcador);
+            }, false);
+        });
+    
+        document.querySelectorAll('.js-nota').forEach(nota => {
+            nota.addEventListener('click', event => {
+                let elementoPai = event.target;
+                if(event.target.tagName !== 'DIV' || event.target.classList.contains('notas')){
+                    elementoPai = event.target.parentElement;
+                }
+                const id = elementoPai.querySelector('.id-nota').value * 1;
+                abrirNota(id);
+            });
+        });
+        
+        document.querySelector('.js-fechar').addEventListener('click', () => {
+            fecharNota();
+        });
+    
+        document.querySelector('.js-arquivar').addEventListener('click', () => {
+            const idElement = document.getElementById('id-nota-aberta');
+            if(!idElement) return;
+    
+            const id = idElement.value * 1;
+    
+            dados = dados.map(dado => {
+                if(dado.id === id){
+                    dado.arquivado = true;
+                }
+                return dado;
+            });
+            fecharNota();
+            formatarNotas(dados);
+        });
+    
+        document.querySelector('.js-excluir').addEventListener('click', () => {
+            const idElement = document.getElementById('id-nota-aberta');
+            if(!idElement) return;
+    
+            const id = idElement.value * 1;
+    
+            dados = dados.map(dado => {
+                if(dado.id === id){
+                    dado.excluido = true;
+                }
+                return dado;
+            });
+            fecharNota();
+            formatarNotas(dados);
+        });
+    
+        document.querySelector('.nota-aberta').addEventListener('click', event => {
+            if(event.target.classList.contains('nota-aberta')) {
+                fecharNota();
+            }
+        });
+
+        document.querySelector('.js-mostrar-tudo').addEventListener('click', () => {
+            formatarNotas(dados);
+        });
+    }
 
     const marcadoresResumidos = extrairMarcadoresUnicos(dados);    
     marcadoresResumidos.forEach(marcador => renderizarMarcadoresMenu(marcador));
     formatarNotas(dados);
-    
-    document.querySelectorAll('.js-marcador').forEach(function(marcador){
-        marcador.addEventListener('click', function(event){
-            let nomeMarcador = '';
-            if(event.target.tagName === 'LI'){
-                nomeMarcador = event.target.querySelector('a').textContent;
-            } else {
-                nomeMarcador = event.target.parentElement.querySelector('a').textContent;
-            }
-            filtrarNotasPorMarcador(dados, nomeMarcador);
-        }, false);
-    });
-
-    document.querySelectorAll('.js-nota').forEach(nota => {
-        nota.addEventListener('click', event => {
-            let elementoPai = event.target;
-            if(event.target.tagName !== 'DIV' || event.target.classList.contains('notas')){
-                elementoPai = event.target.parentElement;
-            }
-            const id = elementoPai.querySelector('.id-nota').value * 1;
-            abrirNota(id);
-        });
-    });
-    
-    document.querySelector('.js-fechar').addEventListener('click', () => {
-        fecharNota();
-    });
-
-    document.querySelector('.nota-aberta').addEventListener('click', event => {
-        if(event.target.classList.contains('nota-aberta')) {
-            fecharNota();
-        }
-    })
 })();
